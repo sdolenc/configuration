bf37f50213bf2702898866dc58bee53048ea7325
Joseph Mulloy <jmulloy@edx.org>
2017-12-22 16:57:38 -0500

    Improve script

--- util/jenkins/check-celery-queues.py
+++ util/jenkins/check-celery-queues.py
@@ -1,55 +1,49 @@
+import json
 import redis
 import click
 import boto3
-import json
-
-host = 'localhost'
+# TODO: Add backoff/retry
 
 @click.command()
-@click.option('--host', '-h', default='localhost', help='Hostname of redis servre')
+@click.option('--host', '-h', default='localhost',
+              help='Hostname of redis server')
 @click.option('--port', '-p', default=6379, help='Port of redis server')
 @click.option('--environment', '-e', required=True)
-@click.option('--deploy', '-d', help="Deployment (i.e. edx or edge)", required=True)
-@click.option('--max-metrics', default=30, help='Maximum number of CloudWatch metrics to publish')
+@click.option('--deploy', '-d', required=True,
+              help="Deployment (i.e. edx or edge)")
+@click.option('--max-metrics', default=30,
+              help='Maximum number of CloudWatch metrics to publish')
 def check_queues(host, port, environment, deploy, max_metrics):
-
-    namespace="celery/{}-{}".format(environment,deploy)
-#    namespace="AWS/EC2"
-
+    namespace = "celery/{}-{}".format(environment, deploy)
     r = redis.StrictRedis(host=host, port=port)
     cw = boto3.client('cloudwatch')
-    
-#    response = cw.list_metrics()
-#    print(json.dumps(response["Metrics"], indent=2, sort_keys=True))
-#    existing_metrics=set([metric["MetricName"] for metric in response["Metrics"]])
-#    print(existing_metrics)
+    metric_name = 'queue_length'
+    dimension = 'queue'
+    response = cw.list_metrics(Namespace=namespace, MetricName=metric_name,
+                               Dimensions=[{'Name': dimension}])
+    print(json.dumps(response["Metrics"], indent=2, sort_keys=True))
+    existing_queues = []
+    for m in response["Metrics"]:
+        existing_queues.extend(
+            [d['Value'] for d in m["Dimensions"] if d['Name'] == dimension])
+    print(existing_queues)
 
-    queues = [ k for k in r.keys() if r.type(k) == b'list' ]
+    queues = [k for k in r.keys() if r.type(k) == b'list']
 
-    # Prevent generating too many clouwatch metrics
-    if len(queues) > max_metrics:
-        sys.exit(1)
-   
-    queue_length = {}
+    metric_data = []
     for queue in queues:
-        queue_length[queue.decode()] = r.llen(queue)
+        metric_data.append({
+            'MetricName': metric_name,
+            'Dimensions': [{
+                "Name": dimension,
+                "Value": queue.decode()
+            }],
+            'Value': r.llen(queue)
+        })
 
-    for queue, length in queue_length.items():
+    print(metric_data)
+    response = cw.put_metric_data(Namespace=namespace, MetricData=metric_data)
 
-        print("{}: {}".format(queue, length))
-        response = cw.put_metric_data(
-            Namespace=namespace,
-            MetricData=[{
-                'MetricName': 'queue_length',
-                'Dimensions': [
-                    {
-                        'Name': 'queue',
-                        'Value': queue
-                    }
-                ],
-                'Value': length
-            }]
-        )
 
 if __name__ == '__main__':
     check_queues()
